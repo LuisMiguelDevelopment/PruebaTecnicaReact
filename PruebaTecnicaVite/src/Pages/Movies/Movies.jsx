d6b6654339087971e5bf51801e47bf6a8943880c
Luis Miguel <luismiguelalvarezmarin109@gmail.com>
2023-10-03 10:35:48 -0500

    feat: :sparkles: seccion de series y movies terminada

--- PruebaTecnicaVite/src/Pages/Movies/Movies.jsx
+++ PruebaTecnicaVite/src/Pages/Movies/Movies.jsx
@@ -3,27 +3,53 @@ import './Movies.css'
 import jsonData from '../../../sample.json'
 const Movies = () => {
   const [movies, setMovies] = useState([]);
+  const [modalAbrir, setModalAbrir] = useState(false);
+  const [seleccionarMovies, setSeleccionarMovies] = useState(null);
 
   useEffect(() => {
     const entrada = jsonData.entries;
 
-    const moviesData = entrada.filter((entry) => entry.programType == "movie")
+    const filtrarMovies = entrada.filter((entry)=>{
+      return entry.programType =="movie" && entry.releaseYear >=2010
+    })
 
-    setMovies(moviesData)
+    const alfabetico = filtrarMovies.sort((a,b)=>{
+      return a.title.localeCompare(b.title);
+     })
+
+    setMovies(alfabetico)
 
   }, [])
+
+  const clickCard = (entry) => {
+    setSeleccionarMovies(entry);
+    setModalAbrir(true); 
+  }
+
+  const cerrarModal = () =>{
+    setModalAbrir(null); 
+  }
 
   return (
     <div>
       <div className="cards">
         {movies.map((entry) => (
-          <div className='card' key={entry.title}>
+          <div  onClick={() => clickCard(entry)} className='card' key={entry.title}>
               <img className='card__img' src={entry.images["Poster Art"].url} alt="" />
               <p className='card__p'>{entry.title}</p>
           </div>
          
         ))}
       </div>
+      {modalAbrir && seleccionarMovies && (
+        <div className="modal">
+          <h2 className='modal__h2'>{seleccionarMovies.title}</h2>
+          <p className='modal__p'>{seleccionarMovies.description}</p>
+          <p className='modal__p'>{seleccionarMovies.releaseYear}</p>
+          <img className='card__img' src={seleccionarMovies.images["Poster Art"].url} alt="" />
+          <button className='card__butoon' onClick={() => cerrarModal()}>Salir</button>
+        </div>
+      )}
 
     </div>
   )
--- PruebaTecnicaVite/src/Pages/Series/Series.jsx
+++ PruebaTecnicaVite/src/Pages/Series/Series.jsx
@@ -1,9 +1,57 @@
 import './Series.css'
+import { useEffect, useState } from 'react';
+import jsonData from '../../../sample.json'
 
 const Series = () => {
+  const [series, setSeries] = useState([]);
+  const [modalAbrir, setModalAbrir] = useState(false);
+  const [seleccionarSeries, setSeleccionarSeries] = useState(null);
+
+  useEffect(() => {
+    const entrada = jsonData.entries;
+
+    const filtrarSeries = entrada.filter((entry) => {
+      return entry.programType === "series" && entry.releaseYear >= 2010
+    })
+
+    const alfabetico = filtrarSeries.sort((a, b) => {
+      return a.title.localeCompare(b.title);
+    })
+
+    setSeries(alfabetico)
+
+  }, [])
+
+  const clickCard = (entry) => {
+    setSeleccionarSeries(entry);
+    setModalAbrir(true); 
+  }
+
+  const cerrarModal = () =>{
+    setModalAbrir(null); 
+  }
+
   return (
-    <div>Series</div>
+    <div>
+      <div className="cards">
+        {series.map((entry) => (
+          <div onClick={() => clickCard(entry)} className='card' key={entry.title}>
+            <img className='card__img' src={entry.images["Poster Art"].url} alt="" />
+            <p className='card__p'>{entry.title}</p>
+          </div>
+        ))}
+      </div>
+      {modalAbrir && seleccionarSeries && (
+        <div className="modal">
+          <h2 className='modal__h2'>{seleccionarSeries.title}</h2>
+          <p className='modal__p'>{seleccionarSeries.description}</p>
+          <p className='modal__p'>{seleccionarSeries.releaseYear}</p>
+          <img className='card__img' src={seleccionarSeries.images["Poster Art"].url} alt="" />
+          <button className='card__butoon' onClick={() => cerrarModal()}>Salir</button>
+        </div>
+      )}
+    </div>
   )
 }
 
-export default Series
+export default Series;
